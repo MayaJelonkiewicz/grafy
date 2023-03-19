e83b273b6d2a855f8d93e28150b8276bc766f908
MayaJelonkiewicz <maya.jelonkiewicz@gmail.com>
2023-03-19 23:04:04 +0100

    Move incorrectly numbered task 2.1 to 2.3

--- main_2.py
+++ main_2.py
@@ -4,7 +4,7 @@ import sys
 from graph import Graph
 
 
-def task1(arguments):
+def task3(arguments):
     input_graph = Graph.parse(arguments.input_representation, sys.stdin.read())
     print(*max(input_graph.find_components(), key=len))
 
@@ -13,15 +13,15 @@ def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="task", required=True)
 
-    subparser_1 = subparsers.add_parser("1")
+    subparser_1 = subparsers.add_parser("3")
     subparser_1.add_argument("-i", "--input-representation",
                              choices=["adjlist", "adjmatrix", "incmatrix"], required=True)
 
     arguments = parser.parse_args()
     arguments.task = int(arguments.task)
 
-    if arguments.task == 1:
-        task1(arguments)
+    if arguments.task == 3:
+        task3(arguments)
 
 
 if __name__ == "__main__":
