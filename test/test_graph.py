4bea77c3c783d736ae809028eaefe7ec4f8f47c9
MayaJelonkiewicz <maya.jelonkiewicz@gmail.com>
2023-03-31 14:53:46 +0200

    Update tests to account for previous commit

--- test/test_graph.py
+++ test/test_graph.py
@@ -6,20 +6,29 @@ from graph import Graph
 class GraphTestCase(TestCase):
     """Test Graph class"""
 
-    def test_representation_conversions(self):
+    def test_parse_with_representation(self):
         representations = ["adjlist", "adjmatrix", "incmatrix"]
 
-        graphs = {}
+        graphs = []
         for representation in representations:
             with open(f"test/resources/graph_representations/{representation}.txt", "r",
                       encoding="utf-8") as file:
-                graphs[representation] = Graph.parse(
-                    representation, file.read())  # type: ignore
+                graphs.append(Graph.parse(
+                    representation, file.read()))  # type: ignore
 
-        for first_representation in representations:
-            for second_representation in representations:
-                self.assertEqual(
-                    graphs[first_representation].convert_to(
-                        second_representation),
-                    graphs[second_representation]
-                )
+        # check if all graphs are equal
+        for g0, g1 in zip(graphs, graphs[1:]):
+            self.assertEqual(g0, g1)
+
+    def test_dump_with_representation(self):
+        representations = ["adjlist", "adjmatrix", "incmatrix"]
+
+        with open("test/resources/graph_representations/adjlist.txt", "r", encoding="utf-8") as file:
+            graph = Graph.parse(
+                "adjlist", file.read())
+
+        for representation in representations:
+            with open(f"test/resources/graph_representations/{representation}.txt", "r",
+                      encoding="utf-8") as file:
+                self.assertEqual(graph.dump(representation),  # type: ignore
+                                 file.read())
