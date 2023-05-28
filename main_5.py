1645fd79ce33c9ff1e44bd54f03feedcc0804d9d
GrzegorzGurdak <80112294+GrzegorzGurdak@users.noreply.github.com>
2023-05-28 17:56:38 +0200

    add task 5.1

--- main_5.py
+++ main_5.py
@@ -2,6 +2,9 @@ import argparse
 import sys
 from graph import WeightedDigraph
 
+def task1(arguments):
+    WDigraph= WeightedDigraph.generate_flow_graph(arguments.n)
+    print(WDigraph.dump())
 
 def task2(arguments):
     WDigraph= WeightedDigraph.parse(sys.stdin.read())
@@ -13,6 +16,9 @@ def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="task", required=True)
 
+    subparser_1 = subparsers.add_parser("1")
+    subparser_1.add_argument("n", type=int)
+
     subparser_2 = subparsers.add_parser("2")
     subparser_2.add_argument("s", type=int)
     subparser_2.add_argument("t", type=int)
@@ -21,7 +27,9 @@ def main():
     arguments = parser.parse_args()
     arguments.task = int(arguments.task)
 
-    if arguments.task == 2:
+    if arguments.task == 1:
+        task1(arguments)
+    elif arguments.task == 2:
         task2(arguments)
     
 
