f2be04c74fcb240bfdf7844da2e445216027e42a
MayaJelonkiewicz <maya.jelonkiewicz@gmail.com>
2023-05-30 11:06:58 +0200

    Restrict page_type argument in 6.1

--- main_6.py
+++ main_6.py
@@ -4,16 +4,20 @@ import math
 
 from graph import WeightedDigraph, Digraph
 
+
 def task1(arguments):
     digraph = Digraph.parse(sys.stdin.read())
 
+    visits = None
     if arguments.page_type == "random":
         visits = digraph.PageRank_Random(arguments.iteration_count)
-    if arguments.page_type == "power":
+    elif arguments.page_type == "power":
         visits = digraph.PageRank_PowerMethod(arguments.iteration_count)
+    assert visits is not None
 
     for v in visits:
         print(f"{v[0]} ==> PageRank = {v[1]}")
+
 
 def task2(arguments):
     points = [tuple(map(int, line.split()))
@@ -35,10 +39,11 @@ def task2(arguments):
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="task", required=True)
-    
+
     subparser_1 = subparsers.add_parser("1")
     subparser_1.add_argument("iteration_count", type=int)
-    subparser_1.add_argument("page_type", type=str)
+    subparser_1.add_argument("page_type", choices=[
+                             "random", "power"], type=str)
 
     subparser_2 = subparsers.add_parser("2")
     subparser_2.add_argument("temperature_iteration_count", type=int)
